dd3b65ebfcb389106c2972ff9dc0b2c493d95c53
Irene <ireneuwishema@gmail.com>
2026-02-05 17:00:33 +0200

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,34 @@
+// this is if else condition in javascript and other things i will work in js is here in work you gave me.
+
+let a = 3;
+let b = 4;
+if(a/b){
+    console.log(5);  
+    
+}else{
+    console.log(0.75);
+}
+
+function ryan(b,c){
+    return b+c
+}
+console.log(ryan(3,4))
+
+let rya = "blue";
+switch (rya) {
+    case "blue":
+        console.log("ryan is confident boy he must be teacher &&  preacher bro .");
+        break;
+
+    default:
+        console.log("ryan he is shap boy");
+        
+        break;
+}
+
+
+
+ 
+
+
+ 
